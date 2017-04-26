9c93128d43a78d32cde69d6585d6ac38ca579ab2
rkaiber <rkaiber@br.ibm.com>
2017-04-26 16:54:23 -0300

    GLL World

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
 				<img class = 'newappIcon' src='images/newapp-icon.png'>
 			</td>
 			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
-				<p class='description'></p> KAISON HACUNAMATATAasdasdacachuahucaiuhciuahciuhaiuchaiuchaiucuihwaiudIBER <span class="blue">PHP Starter Application</span>.
+				<h1 id = "message"><?php echo "Welcome 2 GLL World!"; ?></h1>
+				<p class='description'></p> KAISON HACUNAMATATA<span class="blue">PHP Starter Application</span>.
 			</td>
 		</tr>
 	</table>
