9d620c5053fd67f2bcdb3add85ebc16a90eb44b1
mazelim <mroland@br.ibm.com>
2017-04-26 16:02:58 -0300

    djiuawiuduadwa

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 			</td>
 			<td>
 				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
-				<p class='description'></p> KAIBER <span class="blue">PHP Starter Application</span>.
+				<p class='description'></p> KAdhadwadhuhwaduihwaiudIBER <span class="blue">PHP Starter Application</span>.
 			</td>
 		</tr>
 	</table>
